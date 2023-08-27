55bb4803cb9f67edfcd1cc1d7d15a89f8ac1d574
MaRkos <markous_k@protonmail.com>
2023-08-27 17:30:15 -0300

    commit "alteracoes: private e getters&setters"

--- src/controller/ProdutoController.java
+++ src/controller/ProdutoController.java
@@ -6,38 +6,39 @@ public class ProdutoController {
 
     public static void main(String[] args) {
         Produto produto1 = new Produto("Arroz", 10.0, 100);//declarei uma variavel do tipo 'produto'
-        System.out.println(produto1.nome);
-        System.out.println(produto1.valor);
-        System.out.println(produto1.estoque);
+        System.out.println(produto1.getNome());
+        System.out.println(produto1.getValor());
+        System.out.println(produto1.getEstoque());
         System.out.println();
 
         Produto produto2 = new Produto("Massa", 5.50, 100);//produto2 = instancia da classe produto ou simplesmente um OBJETO
-        System.out.println(produto2.nome);
-        System.out.println(produto2.valor);
-        System.out.println(produto2.estoque);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.getEstoque());
         System.out.println();
 
         Produto produto3 = new Produto();
-        System.out.println(produto3.nome);
-        System.out.println(produto3.valor);
-        System.out.println(produto3.estoque);
+        System.out.println(produto3.getNome());
+        System.out.println(produto3.getValor());
+        System.out.println(produto3.getEstoque());
         System.out.println();
 
         Produto produto4 = new Produto();
-        System.out.println(produto4.nome);
-        System.out.println(produto4.valor);
-        System.out.println(produto4.estoque);
+        System.out.println(produto4.getNome());
+        System.out.println(produto4.getValor());
+        System.out.println(produto4.getEstoque());
 
         //Construtor = chamamos de metodo padrao
 
-        //Introducao ao encapsulamento
+        //Introducao ao encapsulamento (public, private, protected ...)
         //Metodos acessores
+        //MVC (Model View COntrol)
     }
 }
 class Produto{
-    String nome; //tipo classe comeca com letra maiuscula
-    double valor;
-    int estoque;
+    private String nome; //tipo classe comeca com letra maiuscula
+    private double valor;
+    private int estoque;
 
     public Produto() {//COntrutor padrao/vazio
     }
@@ -47,4 +48,36 @@ class Produto{
         this.valor = valor;
         this.estoque = estoque;
     }
-}
+/*    public Produto(String nome){
+        this.nome = nome;
+    }
+*/
+    //============ GETTERS (leem valores dos atrib) & SETTERS (altera os valores do atrbt)  ===========
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        int novoEstoque = this.estoque + estoque;
+        if(this.estoque > 0){
+            this.estoque = estoque;
+        }
+
+    }
+}// FIM CLASSE PRODUTO
